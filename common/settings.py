2db91222f1d005d551f6388760d16b2db338e2ce
losiik <49565548+losiik@users.noreply.github.com>
2025-04-06 11:45:22 +0300

    init alembic migrations

---
+++ common/settings.py
@@ -0,0 +1,32 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    port: int = 8000
+
+    # default subscription period in days
+    period: int = 30
+
+    # postgres settings
+    db_host: str = os.getenv("DB_HOST")
+    db_port: str = os.getenv("DB_PORT")
+    db_main_database: str = os.getenv("DB_MAIN_DATABASE")
+    db_user: str = os.getenv("DB_USER")
+    db_password: str = os.getenv("DB_PASSWORD")
+
+    db_connection_rout: str = f"{db_user}:{db_password}@{db_host}:{db_port}/{db_main_database}"
+
+    sql_alchemy_connection_url: str = f"postgresql+psycopg2://{db_connection_rout}"
+
+    # jwt settings
+    jwt_secret_key: str = os.getenv("JWT_SECRET_KEY")
+    access_expires_delta: int = int(os.getenv("ACCESS_EXPIRES_DELTA"))  # min
+    refresh_expires_delta: int = int(os.getenv("REFRESH_EXPIRE_DELTA"))  # day
+
+    # password hashing
+    salt_postfix: str = os.getenv("SALT_POSTFIX")
+
+
+settings = Settings()
