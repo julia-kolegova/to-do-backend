a3a05a8d3c81c7b6c680e8bc10b6505883f28291
losiik <49565548+losiik@users.noreply.github.com>
2025-05-19 08:36:12 +0300

    fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import uvicorn
 
 from dotenv import load_dotenv
+from yoyo import get_backend, read_migrations
 
 if os.path.exists('.env'):
     load_dotenv('.env')
@@ -12,7 +13,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from common.settings import settings
 from app.api.routers import api_router
 from app.database import AsyncDBSessionMiddleware
-from yoyo import get_backend, read_migrations
 
 
 backend = get_backend(settings.sql_alchemy_connection_url.replace("+asyncpg", ""))
